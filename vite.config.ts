e935062a10560b4c62ed39915d758bb08e4c89d0
Cresiak <133695166+Cresiak@users.noreply.github.com>
2026-01-11 23:44:02 +0700

    Set server base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       server: {
+          base: /Lumina_V2/,
         port: 3000,
         host: '0.0.0.0',
       },
